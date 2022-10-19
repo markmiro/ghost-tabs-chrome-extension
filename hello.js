275bf14e4082f18fd4a84a8b34b8cbf43376cadc
Mark Miro <contact@markmiro.com>
2022-10-18 18:34:47 -0700

    Add debug scripts: reload all, load popular sites

--- hello.js
+++ hello.js
@@ -15,3 +15,40 @@ document.getElementById("reset-all-rules").onclick = async () => {
   });
   alert(`Removed ids: ${ruleIds.join(", ")}.`);
 };
+
+document.getElementById("reload-all").onclick = async () => {
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+  tabs.forEach((tab) => {
+    chrome.tabs.reload(tab.id, { bypassCache: true });
+  });
+};
+
+document.getElementById("open-popular-sites").onclick = async () => {
+  chrome.windows.create({
+    focused: true,
+    // https://en.wikipedia.org/wiki/List_of_most_visited_websites
+    url: [
+      "https://google.com",
+      "https://youtube.com",
+      "https://facebook.com",
+      "https://twitter.com",
+      "https://instagram.com",
+      "https://wikipedia.org",
+      "https://yahoo.com",
+      "https://whatsapp.com",
+      "https://amazon.com",
+      "https://live.com",
+      "https://netflix.com",
+      "https://reddit.com",
+      "https://linkedin.com",
+      "https://vk.com",
+      "https://weather.com",
+      "https://duckduckgo.com",
+      "https://microsoft.com",
+      "https://quora.com",
+      "https://ebay.com",
+      "https://msn.com",
+      "https://booking.com",
+    ],
+  });
+};
