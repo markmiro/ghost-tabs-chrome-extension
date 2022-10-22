3ba58e57df98856c77d9d6e4939f7744f0bed9ba
Mark Miro <contact@markmiro.com>
2022-10-21 20:04:31 -0700

    Remove unused code for debugging

--- background.js
+++ background.js
@@ -20,19 +20,3 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return true; // Return true to indicate async response;
   }
 });
-
-// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-//   if (changeInfo.favIconUrl) {
-//     if (tab.favIconUrl.startsWith('data:')) {
-//       console.log(tabId, 'data:...');
-//     } else {
-//       console.log(tabId, tab.favIconUrl);
-//     }
-//   }
-// });
-
-// Note: URL may not be available
-// https://developer.chrome.com/docs/extensions/reference/tabs/#event-onCreated
-chrome.tabs.onCreated.addListener((tab) => {
-  console.log('TAB CREATED', tab.id, { favIconUrl: tab.favIconUrl });
-})
