899143a1bd1c2b3c2996d03ce28b2f02a56cc6ad
Mark Miro <contact@markmiro.com>
2022-10-26 21:13:48 -0700

    Fix issue with popup interactivity. Make counter and reload all work
    After the `badSites` is matched, the HTML added makes the rest of the HTML elements lose their event handlers.

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,27 @@
-const badSites = [
-  'https://chrome.google.com/webstore',
-  'chrome://extensions'
-]
+{
+  const badSites = [
+    'https://chrome.google.com/webstore',
+    'chrome://extensions'
+  ]
 
-chrome.tabs.query({ currentWindow: true, active: true }, async (tabs) => {
-  const tab = tabs?.[0];
-  const match = badSites.find(url => tab.url.startsWith(url));
-  if (tab && match) {
-    document.body.innerHTML = `<b class="bg-washed-yellow black">⚠️ Page isn't supported: ${match}</b>` + document.body.innerHTML;
-  }
+  chrome.tabs.query({ currentWindow: true, active: true }, async (tabs) => {
+    const tab = tabs?.[0];
+    const match = badSites.find(url => tab.url.startsWith(url));
+    if (tab && match) {
+      document.getElementById('js-page-not-supported').innerHTML = `<b class="db pa3">⚠️ Page isn't supported: ${match}</b><hr />`;
+    }
+  });
+}
+
+let clicks = 0;
+document.getElementById('js-counter').addEventListener('click', e => {
+  clicks++;
+  document.getElementById('js-counter').innerText = 'Clicks: ' + clicks;
 });
+
+document.getElementById("js-reload-all").onclick = async () => {
+  const tabs = await chrome.tabs.query({ currentWindow: true });
+  tabs.forEach((tab) => {
+    chrome.tabs.reload(tab.id, { bypassCache: false });
+  });
+};
