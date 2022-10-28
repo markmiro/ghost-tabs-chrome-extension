ba72f77c35c9861f4f259608fc840e3247130b21
Mark Miro <contact@markmiro.com>
2022-10-27 23:07:05 -0700

    Extract setFavicon to util-dom.js

--- js/util-dom.js
+++ js/util-dom.js
@@ -39,3 +39,47 @@ export async function unreadIcon(url, amount) {
   }
   return unreadIconBase(url, amount);
 }
+
+// https://stackoverflow.com/a/260876
+export function setFavicon(href) {
+  // Remove existing favicons
+  // ---
+  // Chrome will choose the correct icon to display in the tab based on a set of criteria. I could
+  // use the `tab.favIconUrl` value, but this will return an absolute URL, whereas the `href`
+  // in the favicon `<link>` is usually a relative path.
+  //
+  // Chrome seems to choose the SVG url (for css-tricks.com website) in a way that seems to contradict
+  // the info on this page: https://stackoverflow.com/a/28094560
+  //
+  // However, the bigger issue is that by updating that favicon, now we have to consider the possibility
+  // that another favicon might now take precedence. For example, suppose two favicons: an ICO icon and
+  // an SVG icon. After updating the ICO icon to a data URL, now the SVG icon might take precedence over
+  // the data url. To fix this, I would need to remove the SVG icon from the HTML.
+  //
+  // Messing with the links should be find since it's all in the head, and most websites don't change
+  // content up there dynamically.
+  const allFavicons = document.querySelectorAll("link[rel~='icon']");
+  allFavicons.forEach((favicon) => favicon.remove());
+
+  // Create a new link element
+  let link = document.createElement("link");
+  link.rel = "icon";
+  document.getElementsByTagName("head")[0].appendChild(link);
+
+  link.type = "image/png";
+  // https://stackoverflow.com/a/61901020
+  // https://developer.mozilla.org/en-US/docs/Web/API/SecurityPolicyViolationEvent
+  document.addEventListener("securitypolicyviolation", (e) => {
+    console.log('CSP ERROR FROM CONTENT SCRIPT:: event: ', e);
+    // Checking all this to decrease the chance that it was triggered by something else
+    // Possibly add `&& sourceFile === "chrome-extension"`
+    if (e.blockedURI === 'data' && e.violatedDirective === 'img-src') {
+      IS_DATA_URL_BLOCKED = true;
+      // TODO: send this data up to the popup so user can see why this tab doesn't get faded.
+    }
+  }, { once: true });
+  link.href = href;
+
+  // TODO: Consider removing dynamically added links too
+  // https://github.com/Elliot67/env-specific-favicon/blob/main/src/contentScripts/index.ts#L53
+}
