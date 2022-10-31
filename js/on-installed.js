de895955c7a1001c58c39de16bbb4008e00e307f
Mark Miro <contact@markmiro.com>
2022-10-30 23:19:33 -0700

    Close on-installed.html after page reload

--- js/on-installed.js
+++ js/on-installed.js
@@ -1,4 +1,5 @@
 document.getElementById('js-reload-all-tabs').addEventListener('click', async () => {
   const tabs = await chrome.tabs.query({});
   tabs.forEach((tab) => chrome.tabs.reload(tab.id));
+  window.close();
 });
