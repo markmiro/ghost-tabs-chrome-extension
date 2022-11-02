97ed8f00ca55ec32e63b17adc3b7735fe8149166
Mark Miro <contact@markmiro.com>
2022-11-02 13:22:57 -0700

    Comment out unused code, and add todo

--- js/util-debug.js
+++ js/util-debug.js
@@ -10,12 +10,16 @@ export const TEST_PAGES = [
   "http://localhost:3000/jpg-multiple",
 ];
 
-// Sites that 
-export const TEST_SITES = [
-  warningPage,
-  "https://example.com", // no icon
-  "https://github.com/site-map", // svg icon
-];
+// TODO: make `TEST_PAGES` for these since I don't own these sites and they can change how they do favicons at any time
+// Sites that test some edge cases
+// export const TEST_SITES = [
+//   warningPage,
+//   "https://example.com", // no icon
+//   "https://github.com/site-map", // svg icon
+//   "https://css-tricks.com/", // svg icon
+//   "https://microsoft.com", // icon with a query string attached to favIconUrl like this: "...favicon.ico?v=4"
+//   "https://linkedin.com" // icon without easily discernable type: <link rel="icon" href="https://static-exp1.licdn.com/sc/h/akt4ae504epesldzj74dzred8">
+// ];
 
 // Sites that have CSP issues with updating the icon.
 // Require fixing content-security-policy headers.
