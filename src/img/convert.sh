7844d3c372c117424084be03e529f34eaf2018bb
Mark Miro <contact@markmiro.com>
2022-11-11 06:19:40 -0800

    Update ghost tabs icons

--- src/img/convert.sh
+++
@@ -1,3 +0,0 @@
-# Make sure you've installed imagemagick
-# `brew install imagemagick` on macOS with homebrew
-magick convert generated/ghost-extension-icon-32.png generated/ghost-extension-icon-128.png generated/ghost.ico
